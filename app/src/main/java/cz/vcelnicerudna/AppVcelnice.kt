ffd34538ffd16ba67067d6a8ce94c86539264a06
Oleg Yapparov <oleg.yapparov@infor.com>
2018-04-18 11:03:17 +0200

    Remove ACRA, add Crashlytics

--- app/src/main/java/cz/vcelnicerudna/AppVcelnice.kt
+++ app/src/main/java/cz/vcelnicerudna/AppVcelnice.kt
@@ -1,45 +1,6 @@
 package cz.vcelnicerudna
 
 import android.app.Application
-import cz.vcelnicerudna.configuration.APIConstants
-import org.acra.ACRA
-import org.acra.ReportField
-import org.acra.annotation.AcraHttpSender
-import org.acra.config.ACRAConfigurationException
-import org.acra.config.CoreConfigurationBuilder
-import org.acra.config.DialogConfigurationBuilder
-import org.acra.data.StringFormat
-import org.acra.sender.HttpSender
 
 @Suppress("unused")
-@AcraHttpSender(uri = APIConstants.ANDROID_CRASH_POST_URL,
-        httpMethod = HttpSender.Method.POST)
-class AppVcelnice : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        try {
-            val builder = CoreConfigurationBuilder(this)
-                    .setBuildConfigClass(BuildConfig::class.java)
-                    .setReportFormat(StringFormat.JSON)
-                    .setReportContent(
-                            ReportField.STACK_TRACE,
-                            ReportField.APP_VERSION_CODE,
-                            ReportField.ANDROID_VERSION,
-                            ReportField.PHONE_MODEL,
-                            ReportField.USER_COMMENT)
-            builder
-                    .getPluginConfigurationBuilder(DialogConfigurationBuilder::class.java)
-                    .setResText(R.string.crash_text)
-                    .setResTitle(R.string.crash_title)
-                    .setResCommentPrompt(R.string.crash_prompt)
-                    .setResNegativeButtonText(R.string.cancel)
-                    .setResPositiveButtonText(R.string.send)
-                    .setEnabled(true)
-            ACRA.init(this, builder)
-        } catch (e: ACRAConfigurationException) {
-            e.printStackTrace()
-        }
-    }
-}
+class AppVcelnice : Application() {}
